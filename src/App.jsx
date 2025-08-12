49de22a8bea3e982585df4da51a2bc406d6e9cbe
MattCam <matycama925@gmail.com>
2025-08-12 03:00:47 -0300

    tercera-prueba

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import ChatList from './components/ChatList/ChatList';
 import Chat from './pages/Chat';
 import Home from './pages/Home';
 import './css/App.css';
+import Sidebar from './components/Sidebar/SideBar'; 
 
 function ChatLayout() {
   return (
-    <div className="app-layout">
+    <div className="app-layout" style={{ display: "flex" }}>
+      <Sidebar />  
       <ChatList />
       <div style={{ flex: 1 }}>
         <Outlet />
---
+++ src/components/Sidebar/SideBar.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Sidebar.css";
+
+export default function Sidebar() {
+    const shortcuts = [
+        {
+            key: "new",
+            label: "Chats",
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-chat-left-text-fill" viewBox="0 0 16 16">
+                    <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1z" />
+                </svg>
+            ),
+            onClick: () => console.log("Chats"),
+        },
+        {
+            key: "status",
+            label: "Estados",
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bounding-box-circles" viewBox="0 0 16 16">
+                    <path d="M2 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2M0 2a2 2 0 0 1 3.937-.5h8.126A2 2 0 1 1 14.5 3.937v8.126a2 2 0 1 1-2.437 2.437H3.937A2 2 0 1 1 1.5 12.063V3.937A2 2 0 0 1 0 2m2.5 1.937v8.126c.703.18 1.256.734 1.437 1.437h8.126a2 2 0 0 1 1.437-1.437V3.937A2 2 0 0 1 12.063 2.5H3.937A2 2 0 0 1 2.5 3.937M14 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2M2 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2m12 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2" />
+                </svg>
+            ),
+            onClick: () => console.log("Estados"),
+        },
+        {
+            key: "Channels",
+            label: "Canales",
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chat-left-heart" viewBox="0 0 16 16">
+                    <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
+                    <path d="M8 3.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132" />
+                </svg>
+            ),
+            onClick: () => console.log("Canales"),
+        },
+
+        {
+            key: "Communities",
+            label: "Comunidades",
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
+                </svg>
+            ),
+            onClick: () => console.log("Comunidades"),
+        },
+        
+    ];
+
+
+    
+    return (
+        <div className="sidebar">
+            <div className="shortcuts">
+                {shortcuts.map((item) => (
+                    <button
+                        key={item.key}
+                        className="shortcut"
+                        onClick={item.onClick}
+                        title={item.label}
+                    >
+                        {item.icon}
+                        <span className="tooltip">{item.label}</span>
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
+}
