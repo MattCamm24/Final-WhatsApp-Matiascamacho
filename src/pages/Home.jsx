67622b25d93c27a4b515dd820ed15cf08c63a737
MattCam <matycama925@gmail.com>
2025-08-12 19:18:34 -0300

    septima-prueba

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,24 +8,24 @@ export default function Home() {
       justifyContent: 'space-between',
       alignItems: 'center',
       color: '#000000ff',
-      height: '100vh'
+      height: '100vh',
     }}>
       <img src="/public/avatars/Home-Image.png" alt="White-whatsapp-home-image" style={{
         width: '30%',
-        height: 'auto'
+        height: 'auto',
       }} />
       <h2 style={{
         flexGrow: 10,
         marginBottom: 0,
         fontSize: 24,
-        color: '#000000ff'
+        color: '#000000ff',
       }}>Descarga White WhatsApp para Windows</h2>
       <p style={{
         flexGrow: 500,
         marginBottom: 0,
         color: '#3f3d3dff',
         fontSize: 12,
-        fontWeight: 600
+        fontWeight: 600,
       }}>Haz llamadas, comparte tu pantalla y obtén una experiencia más rápida al descargar la aplicación de Windows</p>
     </div>
   );
